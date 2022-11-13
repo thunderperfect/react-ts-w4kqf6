fc4065c67a132e46944e57b52cefaf7a9fe2814a
thunderperfect <michael@method93.com>
2022-11-13 08:27:39 -0500

    Added Select mapping in onFinish

--- EditCategory.tsx
+++ EditCategory.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-
-import { Select, Form, SelectProps } from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
+import { Select, Form, SelectProps, Input, Button } from 'antd';
 const { Option } = Select;
 
 export interface Case {
@@ -27,25 +27,52 @@ const sampleCase: Case = {
     { code: 'abc', description: 'test 1' },
     { code: 'def', description: 'test 2' },
   ],
-  selectedCategoryCodes: ['abc'],
 };
 
 export default function EditCategory() {
   const [form] = Form.useForm();
 
+  const mapCategoriesFromSelected = (values: any) => {
+    let categories = allCategories.filter((cat) =>
+      form.getFieldValue('selectedCategoryCodes').some((c) => c === cat.code)
+    );
+
+    console.log(categories);
+    return categories;
+  };
+
+  const onFinish = (values: any) => {
+    values['categories'] = mapCategoriesFromSelected(values);
+    console.log(values);
+  };
+
+  const modifiedForm = {
+    ...sampleCase,
+    selectedCategoryCodes: sampleCase.categories.map((cat) => cat.code),
+  };
+
   const options: SelectProps['options'] = allCategories.map(
     (cat: category) => ({
-      label: cat.description,
+      label: (
+        <span>
+          <HomeOutlined /> {cat.description}
+        </span>
+      ),
       value: cat.code,
     })
   );
 
   return (
     <React.Fragment>
-      <Form form={form} initialValues={sampleCase}>
+      <Form form={form} initialValues={modifiedForm} onFinish={onFinish}>
+        <Form.Item name="categories" hidden />
+        <Form.Item name="logNumber">
+          <Input />
+        </Form.Item>
         <Form.Item name="selectedCategoryCodes">
           <Select mode="multiple" options={options} />
         </Form.Item>
+        <Button htmlType="submit">Save</Button>
       </Form>
     </React.Fragment>
   );
